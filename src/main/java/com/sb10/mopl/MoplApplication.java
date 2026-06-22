dbae314a87dfa30f0a0de3fd5f55fb2754be2398
mjohn26 <mjohn262626@gmail.com>
2026-06-22 10:29:02 +0900

    chore: 프로젝트 초기 설정 추가

---
+++ src/main/java/com/sb10/mopl/MoplApplication.java
@@ -0,0 +1,13 @@
+package com.sb10.mopl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoplApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MoplApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/sb10/mopl/MoplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb10.mopl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoplApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
