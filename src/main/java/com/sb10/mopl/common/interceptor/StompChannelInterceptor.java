e6e56843a7123dd4ede32e87a6246ce497c886f6
Nooroong <sjiyeon759@gmail.com>
2026-06-23 17:19:45 +0900

    feat: StompChannelInterceptor 작성

---
+++ src/main/java/com/sb10/mopl/common/interceptor/StompChannelInterceptor.java
@@ -0,0 +1,33 @@
+package com.sb10.mopl.common.interceptor;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class StompChannelInterceptor implements ChannelInterceptor {
+
+  @Override
+  public Message<?> preSend(Message<?> message, MessageChannel channel) {
+    StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message,
+      StompHeaderAccessor.class);
+
+    if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+      String sessionId = accessor.getSessionId();
+      System.out.println("새로운 STOMP 연결: " + sessionId);
+    }
+
+    if (StompCommand.DISCONNECT.equals(accessor.getCommand())) {
+      System.out.println("세션 종료 감지");
+    }
+
+    return message;
+  }
+}
+
