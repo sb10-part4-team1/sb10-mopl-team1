afbf98abc2f5cc5c5aa48bdefa29064611b0ff28
PSG-00 <cdjsdj1902@gmail.com>
2026-06-24 13:27:24 +0900

    style: 컨텐츠 테스트 코드에 컨벤션 위반사항을 수정

--- src/test/java/com/sb10/mopl/content/entity/TagTest.java
+++ src/test/java/com/sb10/mopl/content/entity/TagTest.java
@@ -14,20 +14,27 @@ class TagTest {
 
   @Test
   @DisplayName("정상적인 태그 이름을 지정하여 태그를 생성할 시 성공한다")
-  void createSuccessWhenNameIsValid() {
-    // when
-    Tag tag = Tag.create("스릴러");
+  void create_success_whenNameIsValid() {
+    // given 태그 이름을 정상으로 준다
+    String name = "스릴러";
 
-    // then
+    // when 태그를 생성할 때
+    Tag tag = Tag.create(name);
+
+    // that 생성된 객체가 null이 아니고 이름이 일치하는지 무결성 검증한다
     assertNotNull(tag);
     assertEquals("스릴러", tag.getName());
   }
 
   @Test
   @DisplayName("태그 이름이 비어 있거나 공백일 시 생성에 실패하고 예외를 발생시킨다")
-  void createFailWhenNameIsBlank() {
-    // when & then
-    ContentException ex = assertThrows(ContentException.class, () -> Tag.create("   "));
+  void create_fail_whenNameIsBlank() {
+    // given 태그 이름에 공백을 준다
+    String name = "   ";
+
+    // when 태그를 생성할 때
+    // that 예외가 발생하며 에러 맵에 name 필드가 포함되는지 검증한다
+    ContentException ex = assertThrows(ContentException.class, () -> Tag.create(name));
 
     assertEquals(ContentErrorCode.INVALID_CONTENT_DATA, ex.getErrorCode());
     assertTrue(ex.getDetails().containsKey("name"));
@@ -35,11 +42,12 @@ class TagTest {
 
   @Test
   @DisplayName("태그 이름이 100자를 초과할 시 생성에 실패하고 예외를 발생시킨다")
-  void createFailWhenNameTooLong() {
-    // given
+  void create_fail_whenNameTooLong() {
+    // given 100자를 초과하는 태그 이름을 준다
     String longName = "T".repeat(101);
 
-    // when & then
+    // when 태그를 생성할 때
+    // that 예외가 발생하며 에러 맵에 name 필드가 포함되는지 검증한다
     ContentException ex = assertThrows(ContentException.class, () -> Tag.create(longName));
 
     assertEquals(ContentErrorCode.INVALID_CONTENT_DATA, ex.getErrorCode());
