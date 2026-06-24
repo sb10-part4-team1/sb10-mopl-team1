5503463abeca4e836a61aba07303539ecbb61970
jh9dev <sungjuhyun1001@gmail.com>
2026-06-24 14:19:07 +0900

    feat: @Transactional 적용

--- src/main/java/com/sb10/mopl/user/service/UserService.java
+++ src/main/java/com/sb10/mopl/user/service/UserService.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +22,7 @@ public class UserService {
   private final PasswordEncoder passwordEncoder;
   private final UserMapper userMapper;
 
+  @Transactional
   public UserDto signUp(UserCreateRequest userCreateRequest) {
     String name = userCreateRequest.name();
     String email = userCreateRequest.email();
