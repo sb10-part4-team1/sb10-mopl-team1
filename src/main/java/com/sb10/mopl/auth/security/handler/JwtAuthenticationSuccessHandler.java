150fce8f17fc8ff0144e894c36bacaa6f0ce3b77
jh9dev <sungjuhyun1001@gmail.com>
2026-06-26 16:09:18 +0900

    feat: 이메일 로그인 인증 필터 추가

---
+++ src/main/java/com/sb10/mopl/auth/security/handler/JwtAuthenticationSuccessHandler.java
@@ -0,0 +1,41 @@
+package com.sb10.mopl.auth.security.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sb10.mopl.auth.dto.response.JwtDto;
+import com.sb10.mopl.auth.security.jwt.JwtProvider;
+import com.sb10.mopl.auth.security.user.MoplUserDetails;
+import com.sb10.mopl.user.dto.response.UserDto;
+import com.sb10.mopl.user.mapper.UserMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+  private final JwtProvider jwtProvider;
+  private final UserMapper userMapper;
+  private final ObjectMapper objectMapper;
+
+  @Override
+  public void onAuthenticationSuccess(
+      HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+      throws IOException {
+    MoplUserDetails userDetails = (MoplUserDetails) authentication.getPrincipal();
+    UserDto userDto = userMapper.toDto(userDetails.getUser());
+
+    response.setStatus(HttpServletResponse.SC_OK);
+    response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+    response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
+    JwtDto jwtDto = new JwtDto(userDto, jwtProvider.createAccessToken(userDetails));
+    objectMapper.writeValue(response.getWriter(), jwtDto);
+  }
+}
