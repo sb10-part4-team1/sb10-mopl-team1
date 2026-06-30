0bd5d4a1640b92bd17869c9afc0c5108cd5cfac7
PSG-00 <cdjsdj1902@gmail.com>
2026-06-30 10:16:10 +0900

    style: // 주석으로 인해 spotlesscheck와 checkstyle이 충돌하는 문제 수정

--- src/main/java/com/sb10/mopl/batch/sports/dto/SportsContentDto.java
+++ src/main/java/com/sb10/mopl/batch/sports/dto/SportsContentDto.java
@@ -4,9 +4,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record SportsContentDto(
-    String idEvent, // 식별자로 사용할 수도 있음(예비)
-    String strEvent, // Title
-    String strFilename, // Description
-    String strThumb, // ThumbnailUrl
-    String strVenue // Tag
-    ) {}
+    String idEvent, /* 식별자로 사용할 수도 있음(예비) */
+    String strEvent, /* Title */
+    String strFilename, /* Description */
+    String strThumb, /* ThumbnailUrl */
+    String strVenue /* Tag */) {}
