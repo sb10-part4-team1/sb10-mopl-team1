880b34eff82d428d47b51e1b340634cf524ace54
mjohn26 <mjohn262626@gmail.com>
2026-07-02 14:34:53 +0900

    feat: 코드리뷰 리뷰 반영

--- src/main/java/com/sb10/mopl/playlist/dto/PlaylistCreateRequest.java
+++ src/main/java/com/sb10/mopl/playlist/dto/PlaylistCreateRequest.java
@@ -6,4 +6,4 @@ import jakarta.validation.constraints.Size;
 public record PlaylistCreateRequest(
     @NotBlank(message = "플레이리스트 제목은 필수입니다") @Size(max = 255, message = "플레이리스트 제목은 255자 이하여야합니다.")
         String title,
-    @NotBlank(message = "플레이리스트 내용은 필수입니다.") String description) {}
+    @NotBlank(message = "플레이리스트 설명은 필수입니다.") String description) {}
