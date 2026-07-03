12aba52dba2f9dfbf26d6edde808277e4fb97956
mjohn26 <mjohn262626@gmail.com>
2026-07-03 13:25:57 +0900

    feat: 플레이리스트 콘텐츠 엔티티 및 저장 구조 구현

---
+++ src/main/java/com/sb10/mopl/playlistcontent/repository/PlaylistContentRepository.java
@@ -0,0 +1,7 @@
+package com.sb10.mopl.playlistcontent.repository;
+
+import com.sb10.mopl.playlistcontent.entity.PlaylistContent;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlaylistContentRepository extends JpaRepository<PlaylistContent, UUID> {}
