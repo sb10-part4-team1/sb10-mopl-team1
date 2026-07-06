3886f1cb9c7046d84157e358654b9cb780ac568a
jh9dev <sungjuhyun1001@gmail.com>
2026-07-06 23:16:30 +0900

    feat: 사용자 목록 조회 QueryDSL 쿼리 구현

---
+++ src/main/java/com/sb10/mopl/user/repository/UserRepositoryCustom.java
@@ -0,0 +1,12 @@
+package com.sb10.mopl.user.repository;
+
+import com.sb10.mopl.user.dto.request.UserSearchRequest;
+import com.sb10.mopl.user.entity.User;
+import java.util.List;
+
+public interface UserRepositoryCustom {
+
+  List<User> findAllByCondition(UserSearchRequest request);
+
+  long countByCondition(UserSearchRequest request);
+}
