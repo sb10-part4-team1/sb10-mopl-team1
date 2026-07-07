3b692c6fe1e18bd101213baaf57478e5629d5f2a
mjohn26 <mjohn262626@gmail.com>
2026-07-07 11:50:48 +0900

    feat: 팔로ë우 조회 기능 구현

--- src/main/java/com/sb10/mopl/follow/service/FollowService.java
+++ src/main/java/com/sb10/mopl/follow/service/FollowService.java
@@ -9,4 +9,10 @@ public interface FollowService {
   FollowDto follow(UUID followerId, FollowRequest request);
 
   void unfollow(UUID userId, UUID followId);
+
+  // 현재 로그인한 사용자가 특정 유저를 팔로우 중인지 조회
+  FollowDto findFollowedByMe(UUID followerId, UUID followeeId);
+
+  // 특정 유저를 팔로우하는 사람 수 조회
+  long countFollowers(UUID followeeId);
 }
