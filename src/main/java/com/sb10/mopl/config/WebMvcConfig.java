2f187b77c43e41d5bd8e41ec53c472c5c0cc0b61
PSG-00 <cdjsdj1902@gmail.com>
2026-07-08 23:49:32 +0900

    fix: localUpload할 시 src가 아닌 build폴더를 바라보는 문제 수정

--- src/main/java/com/sb10/mopl/config/WebMvcConfig.java
+++ src/main/java/com/sb10/mopl/config/WebMvcConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -16,5 +17,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
   @Override
   public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
     resolvers.add(currentUserArgumentResolver);
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry
+        .addResourceHandler("/uploads/**")
+        .addResourceLocations(
+            "file:" + System.getProperty("user.dir") + "/src/main/resources/static/uploads/");
   }
 }
