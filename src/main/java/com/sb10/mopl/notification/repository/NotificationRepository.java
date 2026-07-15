c7d757978ea13daeb8deb39a625dcfec57945700
Nooroong <sjiyeon759@gmail.com>
2026-07-15 17:56:16 +0900

    feature: NotificationRepository 작성

---
+++ src/main/java/com/sb10/mopl/notification/repository/NotificationRepository.java
@@ -0,0 +1,9 @@
+package com.sb10.mopl.notification.repository;
+
+import com.sb10.mopl.notification.entity.Notification;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, UUID> {}
