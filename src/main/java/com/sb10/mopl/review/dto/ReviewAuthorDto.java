9c6577fdc06a60a5d05d6ce23cb11c8ec671ece8
jh9dev <sungjuhyun1001@gmail.com>
2026-07-22 18:09:20 +0900

    docs: review 패키지 API 문서화

--- src/main/java/com/sb10/mopl/review/dto/ReviewAuthorDto.java
+++ src/main/java/com/sb10/mopl/review/dto/ReviewAuthorDto.java
@@ -1,6 +1,10 @@
 package com.sb10.mopl.review.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.UUID;
 
 // 리뷰 작성자를 표현하기 위한 DTO(응답구조를 맞추기위해 구현)
-public record ReviewAuthorDto(UUID userId, String name, String profileImageUrl) {}
+public record ReviewAuthorDto(
+    @Schema(description = "사용자 ID") UUID userId,
+    @Schema(description = "작성자 이름") String name,
+    @Schema(description = "프로필 이미지 URL") String profileImageUrl) {}
